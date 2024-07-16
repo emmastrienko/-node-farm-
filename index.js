d0133364af1534c3accdfaa6bb80e33aebc8dc11
Emma Strienko <mstrienko@gmail.com>
2024-07-16 10:14:32 +0300

    reading and replacing overview template

--- index.js
+++ index.js
@@ -32,23 +32,42 @@ fs.readFile("input.txt", 'utf-8', (err, data) => {
 
 ///////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.toString().replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if(!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+  return output;
+}
 
 const data = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const tempOverviw = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const tempOverviw = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  "utf-8"
+);
 const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`);
 const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`);
-
-console.log(tempOverviw);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
   // Overview page
   if (pathName === "/" || pathName === "/overviev") {
-    res.writeHead(200, {'Content-type': 'text/html'})
-    res.end(tempOverviw);
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el))
+
+    const output = tempOverviw.replace(/{%PRODUCT_CARDS%}/g, cardsHtml)
+    res.end(output);
 
     // Product page
   } else if (pathName === "/product") {
@@ -70,5 +89,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, () => {
-  console.log('Listening on port 8000');
+  console.log("Listening on port 8000");
 });
