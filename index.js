704b594ab9ab9611d18af3193993b24293b7df68
Emma Strienko <mstrienko@gmail.com>
2024-07-17 17:05:05 +0300

    packajes understanding

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-const replaceTemplate = require('./modules/replaceTemplate');
+const slugify = require("slugify");
+const replaceTemplate = require("./modules/replaceTemplate");
+
 /*
 // Block model
 const fileInput = fs.readFileSync('input.txt', 'utf-8');
@@ -34,16 +36,20 @@ fs.readFile("input.txt", 'utf-8', (err, data) => {
 
 ///////////////////////////
 // SERVER
-
-const data = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
-const dataObj = JSON.parse(data);
-
 const tempOverviw = fs.readFileSync(
   `${__dirname}/templates/overview.html`,
   "utf-8"
 );
 const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`);
 const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`);
+
+const data = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
+console.log(slugify("Fresh Avocados", { lower: true }));
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
